23456a3a2bb07ad4e742692fbc1740febdb4db2d
cliven <quanguanyu@qq.com>
2026-02-23 11:58:15 +0800

    简易编译脚本

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+rmdir /s /q target 2>nul
+mkdir target
+cd tlcpchan
+go build -o ../target/tlcpchan.exe ./cmd/tlcpchan
+cd ..
+cd tlcpchan-cli
+go build -o ../target/tlcpchan-cli.exe
+cd ..
+cd tlcpchan-ui
+call npm run build
+xcopy /e /i /y dist ..\target\ui
+cd ..
